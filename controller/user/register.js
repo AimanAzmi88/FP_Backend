faa5978705029ada5cc7597d95439f0ded70e24f
AimanAzmi18 <aimanazmi1816@gmail.com>
2024-08-16 22:00:42 +0800

    update

--- controller/user/register.js
+++ controller/user/register.js
@@ -1,5 +1,5 @@
 import { pool } from "../../database/connection.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 const insertNewUser = `
 INSERT INTO  users (username, password, email)
